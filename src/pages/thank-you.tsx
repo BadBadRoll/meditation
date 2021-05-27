7c6c75b1ad2ba9b7be5cb67fc0b44cdff55094cb
Baasandorj Janchiv <baasandorj@stora.mn>
2021-05-27 13:09:41 +0800

    navigato to home after session

--- src/pages/thank-you.tsx
+++ src/pages/thank-you.tsx
@@ -1,7 +1,18 @@
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useEffect } from 'react'
 import styles from '../styles/success.module.css'
+import { useRouter } from 'next/router'
 
 const Thankyou: FunctionComponent = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push({
+        pathname: '/'
+      }).catch(e => console.error(e))
+    }, 5000)
+  }, [])
+
   return (
     <div className={styles.container}>
       <div className={styles.main}>
